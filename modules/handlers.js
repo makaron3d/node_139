a3fac3fc9c051e2527db7ff6481394fbd65e79b0
makaron3d <makaron3d@gmail.com>
2017-09-18 18:19:52 +0200

    0115

--- modules/handlers.js
+++ modules/handlers.js
@@ -15,18 +15,12 @@ exports.upload = function (request, response) {
     console.log('Rozpoczynam obsługę żądania upload.');
     var form = new formidable.IncomingForm();
     form.parse(request, function (error, fields, files) {
-        fs.renameSync(files.upload.path, 'test.png');
+        fs.renameSync(files.upload.path, 'https://test.png');
         response.writeHead(200, {"Content-Type": 'text/html'});
         response.write('received image:<br/>');
         response.write("<img src='/show' />");
         response.end();
     });
-};
-
-exports.welcome = function (request, response) {
-    console.log('Rozpoczynam obsługę żądania welcome.');
-    response.write('Witaj na stronie startowej!');
-    response.end();
 };
 
 exports.error = function (request, response) {
