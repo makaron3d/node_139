5d349493d227c897d21730a6aaaa574b226fb208
makaron3d <makaron3d@gmail.com>
2017-09-18 10:32:50 +0200

    0114

--- modules/handlers.js
+++ modules/handlers.js
@@ -16,7 +16,7 @@ exports.upload = function (request, response) {
     var form = new formidable.IncomingForm();
     form.parse(request, function (error, fields, files) {
         fs.renameSync(files.upload.path, 'test.png');
-        response.writeHead(200, {"Content-Type": 'image/png'});
+        response.writeHead(200, {"Content-Type": 'text/html'});
         response.write('received image:<br/>');
         response.write("<img src='/show' />");
         response.end();
